167e9a1f2ba8f27583d031d9f3512d177469b914
Hayn Lyn <dbellick93@gmail.com>
2023-03-10 17:12:47 -0500

    idk if I changed anything

--- primer/index.js
+++ primer/index.js
@@ -1,67 +1,46 @@
-let myObject = {
-	greeting: "Hi, there",
-	name: "idfk",
-	amount: 100,
+let hat = {
+	name: "Hat",
+	_price: 100,
+	priceIncTax: 100 * 1.2,
 
-	getWriter() {
-		return (message) => console.log(`${this.greeting}, ${message}`);
+	set price(newPrice) {
+		this._price = newPrice;
+		this.priceIncTax = this._price * 1.2;
 	},
 
-	writeDetails1: () =>
-		console.log(`${this.name}, ${this.amount}`),
+	get price() {
+		return this._price;
+	},
 
-	writeDetails2() {
-		console.log(`${this.name}, ${this.amount}`)
-	}
-		
 	/*
-	writeMessage(message) {
-		console.log(`${this.greeting}, ${message}`);
-	}
+	writeDetails: () =>
+		console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`);
 	*/
+
+	writeDetails: function() {
+		console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`);
+	}
 };
 
-myObject.writeDetails1();
-myObject.writeDetails2();
-let foo = myObject.writeDetails1;
-let bar = myObject.writeDetails2;
+let boots = {
+	name: "Boots",
+	price: "100",
 
+	get priceIncTax() {
+		return Number(this.price) * 1.2;
+	}
+};
 
-foo();
-bar();
-greeting = "Hello";
+hat.writeDetails = hat.writeDetails.bind(hat);
+hat.writeDetails();
+hat.price = 120;
+hat.writeDetails();
 
-// Here, getWriter's `this` belongs to myObject
-let writer = myObject.getWriter();
-writer("It is raining today");
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
+boots.price = "120";
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
 
-// Here, getWriter's `this` belongs to global, since getWriter was passed as a
-// 	value and that's what happens
-let standAlone = myObject.getWriter;
-let standAloneWriter = standAlone();
-standAloneWriter("It is sunny today");
-
-/*
-** Normal functions either work on a global object or on the object in which they were called.
-	That determines their `this` value.
-	You can bind an object to a normal function through myFunction.bind(myObject).
-
-** Arrow functions' `this` values is always the closest `this` value.
-
-// These two are equal
-myObject.writeMessage("It is sunny today");
-myObject.writeMessage.call(myObject, "It is sunny today");
-
-// Assigning an objects method to another variable could change the outcome.
-let myFunction = myObject.writeMessage;
-myFunction("It is sunny today"); // NOT equal to myObject.writeMessage("It is sunny today");
-
-// Bind the function to an object so that `this` refers to the same thing until explicitly changed
-myObject.writeMessage = myObject.writeMessage.bind(myObject)
-
-// Need to reassign
-myFunction("It is sunny today"); // Keep the same, erroneous, old effect since it bears the old assignment.
-myFunction = myObject.writeMessage;
-myFunction("It is sunny today"); // Now it reads "as intended"
-
-*/
+boots.wD = hat.writeDetails;
+boots.wD();
+boots.wD = hat.writeDetails.bind(boots);
+boots.wD();
---
+++ primer/index_testing.js
@@ -0,0 +1,67 @@
+let myObject = {
+	greeting: "Hi, there",
+	name: "idfk",
+	amount: 100,
+
+	getWriter() {
+		return (message) => console.log(`${this.greeting}, ${message}`);
+	},
+
+	writeDetails1: () =>
+		console.log(`${this.name}, ${this.amount}`),
+
+	writeDetails2() {
+		console.log(`${this.name}, ${this.amount}`)
+	}
+		
+	/*
+	writeMessage(message) {
+		console.log(`${this.greeting}, ${message}`);
+	}
+	*/
+};
+
+myObject.writeDetails1();
+myObject.writeDetails2();
+let foo = myObject.writeDetails1;
+let bar = myObject.writeDetails2;
+
+
+foo();
+bar();
+greeting = "Hello";
+
+// Here, getWriter's `this` belongs to myObject
+let writer = myObject.getWriter();
+writer("It is raining today");
+
+// Here, getWriter's `this` belongs to global, since getWriter was passed as a
+// 	value and that's what happens
+let standAlone = myObject.getWriter;
+let standAloneWriter = standAlone();
+standAloneWriter("It is sunny today");
+
+/*
+** Normal functions either work on a global object or on the object in which they were called.
+	That determines their `this` value.
+	You can bind an object to a normal function through myFunction.bind(myObject).
+
+** Arrow functions' `this` values is always the closest `this` value.
+
+// These two are equal
+myObject.writeMessage("It is sunny today");
+myObject.writeMessage.call(myObject, "It is sunny today");
+
+// Assigning an objects method to another variable could change the outcome.
+let myFunction = myObject.writeMessage;
+myFunction("It is sunny today"); // NOT equal to myObject.writeMessage("It is sunny today");
+
+// Bind the function to an object so that `this` refers to the same thing until explicitly changed
+myObject.writeMessage = myObject.writeMessage.bind(myObject)
+
+// Need to reassign
+myFunction("It is sunny today"); // Keep the same, erroneous, old effect since it bears the old assignment.
+myFunction = myObject.writeMessage;
+myFunction("It is sunny today"); // Now it reads "as intended"
+
+*/
